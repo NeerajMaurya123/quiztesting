4fa78663771f272a7be7c6898b61cb4b1ab1ebd6
NeerajMaurya123 <97353392+NeerajMaurya123@users.noreply.github.com>
2022-02-09 18:57:05 +0530

    mcq test

---
+++ index.js
@@ -0,0 +1,58 @@
+let questions =[
+    {
+        numb: 1,
+        question: "Which of the following is a server-side Java Script object?",
+        answer: "File",
+        option: [
+            "Function",
+            "FileUpload",
+            "File",
+            "Date",
+        ]
+    },
+    {
+        numb: 2,
+        question: " Java script can be used for Storing the form's contents to a database file on the server",
+        answer: "False",
+        option: [
+            "False",
+            "True",
+            "None of these",
+        ]
+    },
+    {
+        numb: 3,
+        question: "To insert a JavaScript into an HTML page, which tag is used?",
+        answer: "< script> ",
+        option: [
+            "< script> ",
+            "< js>",
+            "< javascript>",
+            "< script=’java’>",
+        ]
+
+    },
+    {
+        numb: 4,
+        question: "How does Java Script store dates in objects of Date type?",
+        answer :"The number of milliseconds since January 1st, 1970",
+        option: [
+            "The number of days since January 1st, 1900",
+            "The number of seconds since January 1st, 1970",
+            "The number of picoseconds since January 1st, 1970",
+            "The number of milliseconds since January 1st, 1970",
+        ]
+
+    },
+    {
+        numb: 5,
+        question: "Which type of JavaScript language is ___",
+        answer: "Object-Based",
+        option: [
+            "Object-Oriented",
+            "Object-Based",
+            "Assembly-language",
+            "High-level",
+        ]
+    }
+]
